ef3972c619af2cec4de69d94ff6d44df15928347
Shresthikashyap <sk2929542@gmail.com>
2023-12-15 00:04:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Products from './components/Products/Products'
+
+function App() {
+  return (
+    <>
+      <Navbar bg="primary" expand="sm" varient="dark">
+        <Container>
+          <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Products />
+    </>
+  );
+}
+
+export default App;
