847af0b0b44cc0134bbfdc4e78afa304e6ed93c5
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-10-17 11:16:04 +0530

    route to authentication page if not signed in

--- src/components/Cart/AddToCart.js
+++ src/components/Cart/AddToCart.js
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { Button } from "react-bootstrap";
 import AuthContext from "../../store/auth-context"; 
 import CartContext from "../../store/cart-context"; 
-import './AddToCart.css'; 
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 //import Card from "../UI/Card/Card";
 
@@ -10,6 +9,7 @@ const AddProduct = (props) => {
   const authContext = useContext(AuthContext);
   const cartCntxt = useContext(CartContext);
   const history = useHistory();
+
 
   const addToCart = (event) => {
    //  event.preventDefault();
@@ -20,14 +20,14 @@ const AddProduct = (props) => {
     if (authContext.isLoggedIn) {
       addToCart();
     } else {
-      alert('You need to log in!')
-      history.replace('/login');
+      alert('You need to sign in!')
+      history.replace('/auth');
     }
   };
 
   return (
-    <div className="card">
-      <Button variant="dark" onClick={handleClick} className="card">
+    <div >
+      <Button variant="dark" onClick={handleClick} >
         Add Product
       </Button>
     </div>
